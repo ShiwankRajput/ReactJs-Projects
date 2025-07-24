9955c8130986950b499f0e6147f7fbd28370b431
ShiwankRajput <shiwannnnk@gmail.com>
2025-07-24 23:54:49 +0530

    adding more context to shop page

--- Project-6_Ecommerce-Website/src/Pages/Shop.jsx
+++ Project-6_Ecommerce-Website/src/Pages/Shop.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Hero from '../Components/HeroSection/Hero'
+import Popular from '../Components/Popular/Popular'
 
 const Shop = () => {
   return (
     <div>
       <Hero></Hero>
+      <Popular></Popular>
     </div>
   )
 }
