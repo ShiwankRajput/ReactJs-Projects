f1a7fa6feba8f6a3682500924a0faa84859d86ca
ShiwankRajput <shiwannnnk@gmail.com>
2025-08-18 18:53:25 +0530

    developing frontend for HungerHive

---
+++ Project-7_HungerHive-Food_Delivery/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,61 @@
+
+import "./Navbar.css";
+import { assets } from "../../assets/assets";
+import { useContext, useState } from "react";
+import { Link } from "react-router-dom";
+import { StoreContext } from "../../context/StoreContext";
+
+const Navbar = ({ setShowLogin }) => {
+  const [menu, setMenu] = useState("home");
+  const { getTotalCartAmount } = useContext(StoreContext);
+
+  return (
+    <div className="navbar">
+      <Link to="/react-food-delivery-website/">
+        <h1>HungerHive</h1>
+      </Link>
+      <ul className="navbar-menu">
+        <Link
+          to="/react-food-delivery-website/"
+          onClick={() => setMenu("home")}
+          className={menu === "home" ? "active" : ""}
+        >
+          Home
+        </Link>
+        <Link
+          to="/menu"
+          onClick={() => setMenu("menu")}
+          className={menu === "menu" ? "active" : ""}
+        >
+          Menu
+        </Link>
+        <Link 
+          to="/about" 
+          onClick={() => setMenu("about")} 
+          className={menu === "about" ? "active" : ""}
+        >
+          About
+        </Link>
+        <Link
+          to="/contact"
+          onClick={() => setMenu("contact")}
+          className={menu === "contact" ? "active" : ""}
+        >
+          Contact
+        </Link>
+      </ul>
+      <div className="navbar-right">
+        <img src={assets.search_icon} alt="Search" />
+        <div className="navbar-search-icon">
+          <Link to="/cart">
+            <img src={assets.basket_icon} alt="Cart" />
+          </Link>
+          <div className={getTotalCartAmount() === 0 ? "" : "dot"}></div>
+        </div>
+        <button onClick={() => setShowLogin(true)}>Sign In</button>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
